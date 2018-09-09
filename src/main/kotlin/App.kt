03319e89de3e91688e3753676bb12c5c2c787fec
ongoni <ongoni.fg@gmail.com>
2018-09-09 22:51:23 +0400

    initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,14 @@
+import finalstatemachine.FinalStateMachine
+
+class App {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val machine = FinalStateMachine()
+
+            println("Hello world!")
+        }
+    }
+
+}
